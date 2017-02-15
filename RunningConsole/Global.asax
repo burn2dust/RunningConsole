008963371e59420d6af4570a7ee75347fad123c0
Nicholas K <got2play9@gmail.com>
2017-02-14 18:36:33 -0800

    Add project files.

---
+++ RunningConsole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RunningConsole.MvcApplication" Language="C#" %>
---
+++ RunningConsole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunningConsole.Startup))]
+namespace RunningConsole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
